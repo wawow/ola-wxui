d796c36a9b53b80f98fd93a14fad7e3e883bcbbd
[wawow_haha] <[wawow_haha@126.com]>
2018-05-10 12:13:16 +0800

    整体样式调整

--- pages/grid/index.js
+++ pages/grid/index.js
@@ -9,6 +9,14 @@ Page({
                 img: 'https://github.com/wawow/olapic/raw/master/icon/grids.png',
                 url: '/pages/grid/index',
                 text: '九宫格'
+            }, {
+                img: 'https://github.com/wawow/olapic/raw/master/icon/panel.png',
+                url: '/pages/panel/index',
+                text: '面板'
+            }, {
+                img: 'https://github.com/wawow/olapic/raw/master/icon/steps.png',
+                url: '/pages/steps/index',
+                text: '步骤条'
             }
         ]
     },
